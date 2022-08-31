13f5e94b357ab0be1f2ab6673a949bb3939d2400
Vladimir <76052038+vv-musatov@users.noreply.github.com>
2022-08-31 22:54:01 +0300

    Lesson 4. Complete (#4)

--- todo/settings.py
+++ todo/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "django_filters",
     "corsheaders",
     "authnapp",
     "todoapp",
@@ -52,12 +53,17 @@ INSTALLED_APPS = [
 # Auth model
 AUTH_USER_MODEL = "authnapp.ToDoUser"
 
-# Renderers
+# DRF
 REST_FRAMEWORK = {
     "DEFAULT_RENDERER_CLASSES": [
         "rest_framework.renderers.JSONRenderer",
         "rest_framework.renderers.BrowsableAPIRenderer",
-    ]
+    ],
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend"
+    ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 100,
 }
 
 MIDDLEWARE = [
